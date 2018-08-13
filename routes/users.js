2d4f645edf48d09507f40c8c77b385ff23004f67
Alexandr Kasikov <qsefthuken@gmail.com>
2018-08-13 14:58:48 +0700

    delete old routes in users.js

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,4 @@
 let express = require('express');
 let router = express.Router();
 
-let authenticateController = require('../controllers/authenticate-controller');
-let registerController = require('../controllers/register-controller');
-
-router.post('/register', registerController.register);
-router.post('/authenticate', authenticateController.authenticate);
-
 module.exports = router;
